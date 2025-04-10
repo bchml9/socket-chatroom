d4678a332adf828a43b060a96d35a09316878aaa
me <bchml@pm.me>
2025-04-11 02:15:05 +0300

    commit 2

--- socket server/main.cpp
+++ socket server/main.cpp
@@ -1,26 +1,107 @@
+#pragma comment(lib, "ws2_32.lib")
 #include <winsock2.h>
 #include <Ws2tcpip.h>
 #include <iostream>
-#include <string>
 #include <tchar.h>
-#pragma comment(lib, "ws2_32.lib")
+#include <string>
+#include <thread>
+#include <functional>
 
-#define SERVER_ADDRESS "192.168.1.133" 
 #define PORT 27015
+#define HOST "192.168.1.133"
 #define BUFFER_SIZE 1024
 
-/*
-Initialize Winsock.
-Create a socket.
-Bind the socket.
-Listen on the socket for a client.
-Accept a connection from a client.
-Receive and send data.
-Disconnect. 
-*/
+void receiveMessages(SOCKET client_sock) {
+	char chat_buffer[BUFFER_SIZE];
+	int recv_size;
 
+	while (true) {
+		// receive data from server
+		recv_size = recv(client_sock, chat_buffer, sizeof(chat_buffer), 0);
+		if (recv_size == SOCKET_ERROR) {
+			std::cout << "recv() failed: " << WSAGetLastError() << std::endl;
+			break;
+		}
+		else if (recv_size == 0) {
+			std::cout << "server has disconnected :(" << std::endl;
+			break;
+		}
+
+		// null-terminate + server response
+		chat_buffer[recv_size] = '\0';
+		std::cout << "\nServer: " << chat_buffer << std::endl;
+
+		std::cout << "Client: ";
+		std::cout.flush();
+	}
+	closesocket(client_sock);
+}
+
+void sendMessages(SOCKET client_sock) {
+	while (true) {
+		std::cout << "\rClient: ";
+		std::cout.flush();
+
+		std::string message;
+		std::getline(std::cin, message);
+
+		if (message == "exit") {
+			std::cout << "exiting..." << std::endl;
+			break;
+		}
+
+		send(client_sock, message.c_str(), message.length(), 0);
+	}
+	closesocket(client_sock);
+}
 
 int main(const int argc, const char* argv[]) {
+	bool running = true;
+	// initialize winsock
+	WSAData wsaData;
+	WORD wVersionRequested = MAKEWORD(2, 1);
+	if (WSAStartup(wVersionRequested, &wsaData) != 0)
+		ExitProcess(EXIT_FAILURE);
+
+	// create the socket
+	SOCKET client_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (client_sock == INVALID_SOCKET) {
+		std::cout << "error at socket(): " << WSAGetLastError() << std::endl;
+		WSACleanup();
+		ExitProcess(EXIT_FAILURE);
+	}
+	else {
+		std::cout << "socket is working :)" << std::endl;
+	}
+
+	// setup server connection info
+	SOCKADDR_IN server_addr;
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	InetPton(AF_INET, _T(HOST), &server_addr.sin_addr.s_addr);
+
+	// connect to the server
+	if (connect(client_sock, (sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+		std::cout << "connect() failed: " << WSAGetLastError() << std::endl;
+		closesocket(client_sock);
+		WSACleanup();
+		ExitProcess(EXIT_FAILURE);
+	}
+	else {
+		std::cout << "connected to server :)" << std::endl;
+	}
+
+	// create new threads to listen for messages from the server, and send
+	std::thread send_thread(sendMessages, client_sock);
+	std::thread receive_thread(receiveMessages, client_sock);
 
 
+	send_thread.join();
+	receive_thread.join();
+
+	closesocket(client_sock);
+	WSACleanup();
+
+	return 0;
 }
+
