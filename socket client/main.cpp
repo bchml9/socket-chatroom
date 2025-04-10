c2b197de92d49f615d6b598789425f016e323fa3
serendipity <serendipity@OTEUX0HLFM>
2025-04-11 01:23:45 +0300

    Add project files.

---
+++ socket client/main.cpp
@@ -0,0 +1,22 @@
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <iostream>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
+
+#define SERVER_ADDRESS "192.168.1.133" 
+#define PORT 27015
+#define BUFFER_SIZE 1024
+
+/*
+Initialize Winsock.
+Create a socket.
+Connect to the server.
+Send and receive data.
+Disconnect.
+*/
+
+
+int main() {
+	
+}
---
+++ socket server/main.cpp
@@ -0,0 +1,25 @@
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#include <iostream>
+#include <string>
+#pragma comment(lib, "ws2_32.lib")
+
+#define SERVER_ADDRESS "192.168.1.133" 
+#define PORT 27015
+#define BUFFER_SIZE 1024
+
+/*
+Initialize Winsock.
+Create a socket.
+Bind the socket.
+Listen on the socket for a client.
+Accept a connection from a client.
+Receive and send data.
+Disconnect. 
+*/
+
+
+int main() {
+
+
+}
